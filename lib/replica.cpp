c2a827adf221244fe79964b6cecb59c1f9035d01
Tenjou <arthur.der.shefer@gmail.com>
2018-08-29 09:03:17 +0300

    Update cpp runtime library

--- lib/replica.cpp
+++ lib/replica.cpp
@@ -19,4 +19,36 @@ struct Math {
     }
 };
 
+template<class T>
+struct Array {
+    T *buffer;
+    int length;
+    
+    T &operator[](unsigned int index) {
+        return (index > length) ? nullptr : this->buffer[index];
+    }       
+};
+
+struct Float32Array {
+    float *buffer;
+    int length;
+
+    Float32Array() {
+        this->buffer = nullptr;
+        this->length = 0;
+    }
+
+    Float32Array(unsigned int size) {
+        this->buffer = new float[size];
+        this->length = size;
+    }
+
+    Float32Array(Array<double> src) {
+        this->buffer = new float[src.length];
+        for(int n = 0; n < src.length; n++) {
+            this->buffer[n] = static_cast<float>(src.buffer[n]);
+        }
+    }
+};
+
 const double Math::PI = M_PI;
