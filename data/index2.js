254d228042c0362f77e06c0328b29e367a3ece72
Tenjou <arthur.der.shefer@gmail.com>
2018-09-03 08:34:10 +0300

    Update webgl externs

--- data/index2.js
+++ data/index2.js
@@ -1,11 +1,28 @@
 import Matrix4 from "./math/Matrix4"
 
-// let matrixProjection = new Matrix4()
-// let matrixModelView = new Matrix4()
+let matrixProjection = new Matrix4()
+let matrixModelView = new Matrix4()
 let canvas = null
+let gl = null
 
 const create = () => {
 	canvas = document.createElement("canvas")
+	gl = canvas.getContext("webgl")
+	if(!gl) {
+		console.error("Unable to initialize WebGL. Your browser or machine may not support it.")
+		return
+	}
+
+	setupWebGL()
+}
+
+const setupWebGL = () => {
+	gl.clearColor(0.0, 0.0, 0.0, 1.0)
+	gl.clearDepth(1.0)
+	gl.enable(gl.DEPTH_TEST)
+	gl.depthFunc(gl.LEQUAL)
+	gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
+	gl.viewport(0, 0, canvas.clientWidth, canvas.clientHeight)
 }
 
 create()
--- src/Extern.js
+++ src/Extern.js
@@ -11,112 +11,127 @@ let objectType
 let arrayType
 
 const declareClass = (module, name, members, flags = 0) => {
-    const moduleScope = module.scope
-    const scope = new Scope(moduleScope)
-    const node = {
-        id: {
-            name
-        },
-        type: "ClassDeclaration",
-        scope,
-        flags,
-        primitive: PrimitiveType.Class,
-        isType: true
-    }
-    node.varType = node
-    moduleScope.vars[name] = node
+	const moduleScope = module.scope
+	const scope = new Scope(moduleScope)
+	const node = {
+		id: {
+			name
+		},
+		type: "ClassDeclaration",
+		scope,
+		flags,
+		primitive: PrimitiveType.Class,
+		isType: true
+	}
+	node.varType = node
+	moduleScope.vars[name] = node
 
-    for(let key in members) {
-        const member = members[key]
-        scope.vars[key] = member
-    }
-    return node
+	for(let key in members) {
+		const member = members[key]
+		scope.vars[key] = member
+	}
+	return node
 }
 
 const declareType = (module, type, primitive, flags = 0) => {
-    const typeNode = {
-        name: type,
-        primitive,
-        flags,
-        isType: true
-    }
-    module.scope.vars[type] = typeNode
-    return typeNode
+	const typeNode = {
+		name: type,
+		primitive,
+		flags,
+		isType: true
+	}
+	module.scope.vars[type] = typeNode
+	return typeNode
+}
+
+const createVar = (type) => {
+	return {
+		type: "Literal",
+		varType: type,
+		isStatic: true
+	}
 }
 
 const createFunc = (params, returnType = unknownType) => {
-    const signatures = createParams(params)
-    return {
-        signatures,
-        parsed: true,
-        varType: functionType,
-        returnType
-    }
+	const signatures = createParams(params)
+	return {
+		signatures,
+		parsed: true,
+		varType: functionType,
+		returnType
+	}
 }
 
 const createParams = (params) => {
-    const signatures = []
-    for(let n = 0; n < params.length; n++) {
-        const srcBuffer = params[n]
-        const buffer = new Array(srcBuffer.length)
-        for(let m = 0; m < srcBuffer.length; m++) {
-            buffer[m] = {
-                varType: srcBuffer[m]
-            }
-        }
-        signatures.push({
-            params: buffer,
-            paramsRequired: srcBuffer.length
-        })
-    }
-    return signatures
+	const signatures = []
+	for(let n = 0; n < params.length; n++) {
+		const srcBuffer = params[n]
+		const buffer = new Array(srcBuffer.length)
+		for(let m = 0; m < srcBuffer.length; m++) {
+			buffer[m] = {
+				varType: srcBuffer[m]
+			}
+		}
+		signatures.push({
+			params: buffer,
+			paramsRequired: srcBuffer.length
+		})
+	}
+	return signatures
 }
 
 const declareStd = (module) => {
-    unknownType = declareType(module, "Unknown", PrimitiveType.Unknown)
-    numberType = declareType(module, "Number", PrimitiveType.Number)
-    booleanType = declareType(module, "Boolean", PrimitiveType.Boolean)
-    stringType = declareType(module, "String", PrimitiveType.String)
-    functionType = declareType(module, "Function", PrimitiveType.Function)
-    objectType = declareType(module, "Object", PrimitiveType.Object)
-    arrayType = declareType(module, "Array", PrimitiveType.Array, TypeFlag.Array)
+	unknownType = declareType(module, "Unknown", PrimitiveType.Unknown)
+	numberType = declareType(module, "Number", PrimitiveType.Number)
+	booleanType = declareType(module, "Boolean", PrimitiveType.Boolean)
+	stringType = declareType(module, "String", PrimitiveType.String)
+	functionType = declareType(module, "Function", PrimitiveType.Function)
+	objectType = declareType(module, "Object", PrimitiveType.Object)
+	arrayType = declareType(module, "Array", PrimitiveType.Array, TypeFlag.Array)
 
-    const webglContext = declareClass(module, "WebGLRenderingContext", {
+	const webglContext = declareClass(module, "WebGLRenderingContext", {
+		clear: createFunc([[ numberType ]]),
+		clearColor: createFunc([[ numberType, numberType, numberType, numberType ]]),
+		clearDepth: createFunc([[ numberType ]]),
+		enable: createFunc([[ numberType ]]),
+		disable: createFunc([[ numberType ]]),
+		depthFunc: createFunc([[ numberType ]]),
+		viewport: createFunc([[ numberType, numberType, numberType, numberType ]]),
+		DEPTH_TEST: createVar(numberType),
+		LEQUAL: createVar(numberType),
+		COLOR_BUFFER_BIT: createVar(numberType),
+		DEPTH_BUFFER_BIT: createVar(numberType)
+	})
 
-    })
+	const htmlCanvas = declareClass(module, "HTMLCanvasElement", {
+		getContext: createFunc([[ stringType ]], webglContext),
+		clientWidth: createVar(numberType),
+		clientHeight: createVar(numberType)
+	})
 
-    const htmlCanvas = declareClass(module, "HTMLCanvasElement", {
-        getContext: createFunc([[ stringType ]], webglContext)
-    })
+	declareClass(module, "document", {
+		createElement: createFunc([[ stringType ]], htmlCanvas),
+	})
 
-    declareClass(module, "document", {
-        createElement: createFunc([[ stringType ]], htmlCanvas),
-    })
+	declareClass(module, "console", {
+		log: createFunc([[ stringType ]]),
+		error: createFunc([[ stringType ]])
+	})
 
-    declareClass(module, "console", {
-        log: createFunc([[ stringType ]]),
-        error: createFunc([[ stringType ]])
-    })
+	declareClass(module, "Math", {
+		PI: createVar(numberType),
+		sqrt: createFunc([[ numberType ]], numberType),
+		min: createFunc([[ numberType, numberType ]], numberType),
+		max: createFunc([[ numberType, numberType ]], numberType)
+	})
 
-    declareClass(module, "Math", {
-        PI: {
-            type: "Literal",
-            varType: numberType,
-            value: 3.141592653589793,
-            isStatic: true
-        },
-        sqrt: createFunc([[ numberType ]], numberType),
-        min: createFunc([[ numberType, numberType ]], numberType),
-        max: createFunc([[ numberType, numberType ]], numberType)
-    })
-
-    declareClass(module, "Float32Array", {
-        constructor: createFunc([
-            [],
-            [ numberType ],
-            [ arrayType ]
-        ])
-    }, TypeFlag.Array)
+	declareClass(module, "Float32Array", {
+		constructor: createFunc([
+			[],
+			[ numberType ],
+			[ arrayType ]
+		])
+	}, TypeFlag.Array)
 }
 
 export { declareStd }
