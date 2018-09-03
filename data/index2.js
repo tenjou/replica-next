39d9f53ae33c544d1259eeb54b960a974e3099cd
Tenjou <arthur.der.shefer@gmail.com>
2018-09-04 00:30:28 +0300

    Update test example

--- data/index2.js
+++ data/index2.js
@@ -16,6 +16,7 @@ const create = () => {
 	}
 
 	setupWebGL()
+	initShaderProgram(basicVS, basicFS)
 }
 
 const setupWebGL = () => {
@@ -25,6 +26,30 @@ const setupWebGL = () => {
 	gl.depthFunc(gl.LEQUAL)
 	gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
 	gl.viewport(0, 0, canvas.clientWidth, canvas.clientHeight)
+}
+
+const initShaderProgram = (vsSource, fsSource) => {
+	const vertexShader = loadShader(gl.VERTEX_SHADER, vsSource)
+	const fragmentShader = loadShader(gl.FRAGMENT_SHADER, fsSource)
+
+	const program = gl.createProgram()
+	gl.attachShader(program, vertexShader)
+	gl.attachShader(program, fragmentShader)
+	gl.linkProgram(program)	
+}
+
+const loadShader = (type, source) => {
+	const shader = gl.createShader(type)
+	gl.shaderSource(shader, source)
+	gl.compileShader(shader)
+
+	if(!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+		// console.error(`An error occurred compiling the shaders: ${gl.getShaderInfoLog(shader)}`)
+		gl.deleteShader(shader)
+		return null
+	}	
+
+	return shader
 }
 
 const render = () => {
