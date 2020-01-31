efc89405e71206b18232c5fcd7c4d93d8de33ef4
Tenjou <arthur.der.shefer@gmail.com>
2020-01-31 18:13:58 +0200

    update: safety cheks for watcher

--- src/service/WatcherService.js
+++ src/service/WatcherService.js
@@ -1,28 +1,44 @@
 import fs from "fs"
 
+const filesWatching = {}
+const modulesWatching = {}
 let funcListener = null
 
 const watchFile = (file) => {
+	if(filesWatching[file.fullPath]) {
+		return
+	}
 	fs.watch(file.fullPath, { encoding: "utf8" }, (eventType, filename) => {
 		if(filename) {
 			handleWatcherEvent(eventType, file)
 		}
 	})
+	filesWatching[file.fullPath] = true
 }
 
 const unwatchFile = (file) => {
+	if(!filesWatching[file.fullPath]) {
+		return
+	}
 	fs.unwatch(file.fullPath)
 }
 
 const watchModule = (module) => {
+	if(modulesWatching[module.path]) {
+		return
+	}
 	fs.watch(module.path, { encoding: "utf8" }, (eventType, filename) => {
 		if(filename) {
 			handleWatcherEvent(eventType, module)
 		}
 	})
+	modulesWatching[module.path] = true
 }
 
 const unwatchModule = (module) => {
+	if(!modulesWatching[module.path]) {
+		return
+	}
 	fs.unwatch(module.path)
 }
 
